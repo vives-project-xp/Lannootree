4f5c102f7749a014c659e9a172b8f9046e36030f
madness__007 <arno.schoutteten@gmail.com>
2022-09-29 21:28:03 +0200

    frontend in compose

--- API/index.js
+++
@@ -1,9 +0,0 @@
-import Controller from "./controller/controller.js";
-import BackendApi from "./backend-api/backend-api.js";
-import LowEnd from "./low-end/low-end.js";
-import Processor from "./processor/processor.js"
-
-controller = new Controller();
-backendApi = new BackendApi();
-lowEnd = new LowEnd();
-processor = new Processor();
