e99c99155c322bec9b17dc8d5f688b58c76d3767
JensVA <jens.vanhove99@gmail.com>
2022-10-07 00:22:05 +0200

    reworked controller

--- Lannootree_firmware/controller/controller.js
+++ Lannootree_firmware/controller/controller.js
@@ -5,128 +5,157 @@ import mqtt from "mqtt"
 const client = mqtt.connect('mqtt://vps.arnoschoutteten.be');
 
 client.on('connect', function () {
-    console.log("connected")
+    console.log("connected");
+    client.publish('status/controller', 'Online');
+    
+    client.subscribe('controller/matrixsize');
+    client.subscribe('controller/stop');
+    client.subscribe('controller/pause');
+    client.subscribe('controller/play');
+    client.subscribe('controller/togglepause');
+    client.subscribe('controller/setcolor');
+    client.subscribe('controller/effect');
+    client.subscribe('controller/asset');
+    
+
+    client.publish('controller/matrixsize', JSON.stringify({"rows": 5,"columns": 5}));  // DEBUGGING
 })
 
-client.subscribe('controller/stop');
-client.subscribe('controller/pause');
-client.subscribe('controller/setcolor');
-client.subscribe('controller/effect');
-client.subscribe('controller/asset');
-
 client.on('message', function (topic, message) {
-    //console.log(topic)
-    if(topic=="controller/stop") console.log("STOP");
-    else if(topic=="controller/pause") console.log("PAUSE");
-    else if(topic=="controller/setcolor") {
-        console.log(message.toString());
-        const json_obj = JSON.parse(message.toString());
-        console.log(json_obj.red)
-        console.log(json_obj.green)
-        console.log(json_obj.blue)
+    if(topic=="controller/matrixsize") {
+        set_matrixsize(JSON.parse(message.toString()).rows, JSON.parse(message.toString()).columns);
     }
-    else if(topic=="controller/effect") console.log("EFFECT");
+    else if(topic=="controller/stop") stop();
+    else if(topic=="controller/pause") pause();
+    else if(topic=="controller/play") play();
+    else if(topic=="controller/togglepause") togglepause();
+    else if(topic=="controller/setcolor") {
+        playing_effect = null;
+        const json_obj = JSON.parse(message.toString());
+        set_color_full(json_obj.red, json_obj.green, json_obj.blue);
+        frame_to_ledcontroller();
+
+    }
+    else if(topic=="controller/effect") {
+        const json_obj = JSON.parse(message.toString());
+        playing_effect = json_obj.effect_id;
+        play();
+    }
     else if(topic=="controller/asset") console.log("ASSET");
     else console.log("Unknown topic")
-    client.end()
 })
 
 // CONTROLLER
 
-// var ispaused = false;
-// var preprocessed= null;
-// var effect = null;
-// var ledmatrix = Array.from(Array(10), () => new Array(10));
+var ledmatrix = [];
 
-// function main() {
-//     if(!ispaused) {
-//         if(preprocessed != null) play_preprocessed(preprocessed);
-//         if(effect != null) play_effect(effect);
-//         client.publish('controller/stop', "false");
+function set_matrixsize(rows, columns) {
+    pause();
+    if(!isNaN(rows) && !isNaN(columns)) {
+        ledmatrix = Array.from(Array(Math.abs(rows)), () => new Array(Math.abs(columns)));
+        const offcolor = new Color(0,0,0);
+        for(var i = 0; i < ledmatrix.length; i++) {
+            for(var j = 0; j < ledmatrix[i].length; j++) {
+                ledmatrix[i][j] = offcolor;
+            }
+        }
+        console.log(`NEW MATRIX SIZE: \tROWS: ${Math.abs(rows)}, COLUMNS: ${Math.abs(columns)}`);
+    }
+    if(playing_effect != null) play();
+}
+
+var ispaused = true;
+function pause() {
+    ispaused = true;
+}
+function play() {
+    ispaused = false;
+}
+function togglepause() {
+    ispaused = !ispaused;
+}
+
+function frame_to_ledcontroller() {
+    // ------------------------------------
+    // CODE FRAME STUREN NAAR LEDCONTROLLER
+
+
+
+    // ------------------------------------
     
-        
     
-//         // Debugging
-//         // for(var i = 0; i < ledmatrix.length; i++) {
-//         //     for(var j = 0; j < ledmatrix[i].length; j++) {
-//         //         console.log(ledmatrix[i][j]);
-//         //     }
-//         // }
-//     }
-// }
+    frame_to_console(); // DEBUGGING
+}
 
-// function set_matrixsize(width, height) {
-//     stop()
-//     ledmatrix = Array.from(Array(height), () => new Array(width));
-// }
+function frame_to_console() { // DEBUGGING
+    var frame_console = "";
+    for(var i = 0; i < ledmatrix.length; i++) {
+        for(var j = 0; j < ledmatrix[i].length; j++) {
+            if(ledmatrix[i][j] instanceof Color) {
+                var rgb = ledmatrix[i][j].get_color();
+                frame_console+=`(${rgb[0]},${rgb[1]},${rgb[2]})` 
+            }
+        }
+        frame_console+="\n";
+    }
+    console.log(frame_console);
+}
 
-// function stop(){
-//     effect=null;
-//     preprocessed=null;
-//     const off = new Color(0,0,0);
-//     for(var i = 0; i < ledmatrix.length; i++) {
-//         for(var j = 0; j < ledmatrix[i].length; j++) {
-//             ledmatrix[i][j] = off;
-//         }
-//     }
-// }
+function set_color_full(red, green, blue) {
+    if(!isNaN(red) && !isNaN(green) && !isNaN(blue)) {
+        if(red<=255 && green<=255 && blue<=255 && red>=0 && green>=0 && blue>=0) {
+            for(var i = 0; i < ledmatrix.length; i++) {
+                for(var j = 0; j < ledmatrix[i].length; j++) {
+                    ledmatrix[i][j].set_color(red, green, blue);
+                }
+            }
+        }
+    }
+}
 
-// function pause(){
-//     ispaused = !ispaused;
-// }
+var playing_effect = null;
 
-// function get_preprocessed(){
+var effect1_counter = 0;
+var effect2_counter = 0;
+var effect3_counter = 0;
+var effect4_counter = 0;
 
-// }
+function play_effect() {
+    if(playing_effect == "effect1") {       //RANDOM KLEUREN (VOLLEDIG PANEEL)
+        set_color_full(Math.floor(Math.random()*256), Math.floor(Math.random()*256), Math.floor(Math.random()*256));
+        console.log("EFFECT1")
+    }
+    else if(playing_effect == "effect2") {  //RANDOM KLEUREN (IEDERE LED APART)
+        for(var i = 0; i < ledmatrix.length; i++) {
+            for(var j = 0; j < ledmatrix[i].length; j++) {
+                ledmatrix[i][j].set_color(Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256));
+            }
+        }
+        console.log("EFFECT2")
+    }
+    else {
+        pause();
+        playing_effect = null;
+        set_color_full(100,0,0);
+        frame_to_ledcontroller();
+    }
+}
 
-// function set_preprocessed(preprocessed_id){
-//     preprocessed = preprocessed_id
-// }
+function stop(){
+    pause();
+    playing_effect = null;
+    set_color_full(0,0,0);
+    frame_to_ledcontroller();
+}
 
-// function play_preprocessed(){
-
-// }
-
-// function set_color(red, green, blue) {
-
-// }
-
-// function set_effect(effect_id){
-//     effect = effect_id;
-// }
-
-// var index = 0;
-// function play_effect(){
-//     if(effect == "eff1") {
-
-
-//     }
-
-// }
-
-// function get_client_json(){
-//     return [ 
-//         { eff_id: "eff_1", eff_name: ""},
-//         { eff_id: "eff_2", eff_name: ""},
-//         { eff_id: "eff_3", eff_name: ""}
-//     ]
-// }
-
-// function updateMatrix() {
-//     if(!ispaused) {
-//         if(preprocessed != null) play_preprocessed(preprocessed);
-//         if(effect != null) play_effect(effect);
-
-//         client.publish('controller/stop', "false");
-    
-//         // Debugging
-//         // for(var i = 0; i < ledmatrix.length; i++) {
-//         //     for(var j = 0; j < ledmatrix[i].length; j++) {
-//         //         console.log(ledmatrix[i][j]);
-//         //     }
-//         // }
-
-//     }
-// }
-
-// setInterval(main, 1000);
+setInterval(() => {
+    if(!ispaused) {
+        if(playing_effect != null) {
+            play_effect();
+            frame_to_ledcontroller();
+        }
+    }
+    else {
+        console.log("PAUSED");
+    }
+}, 2000);
