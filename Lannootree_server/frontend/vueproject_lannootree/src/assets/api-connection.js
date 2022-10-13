24cb17bdbcddefa0ebe6492a31b98308e27c51da
EmielCoucke <emiel.coucke@me.com>
2022-10-13 14:17:14 +0200

    effect menu

--- Lannootree_server/frontend/vueproject_lannootree/src/assets/api-connection.js
+++ Lannootree_server/frontend/vueproject_lannootree/src/assets/api-connection.js
@@ -5,27 +5,31 @@ export default function websocketClient() {
   ws.onopen = (event) => {
     console.log(event)
     console.log("we are connected to mqtt")
-    // ws.send("Hi, from the client.");
-    // ws.send("stop");
-
+    // ws.send("Hello from from client.");
   }   
-  
 };
+websocketClient();
 
 
 export function Pause(notPaused) {
-  ws.send(notPaused);
+  ws.send(JSON.stringify({"pause": notPaused}));
   console.log(notPaused);
 };
 
-export function stop() {
-  ws.send();
-  console.log(notPaused);
+export function Stop() {
+  ws.onopen = () => ws.send(JSON.stringify({"stop": true}));
+  console.log("stop!")
 };
 
+Stop();
 export function Color() {
-  ws.send("effect");
-  console.log('test');
+ 
+};
+
+export function Effects(selectedEffect) {
+  ws.onopen = () => ws.send("{stop: " + selectedEffect + "}");
+
+ 
 };
 
 // document.getElementById("test") = onOff();
@@ -39,8 +43,7 @@ export function Color() {
 // }
 
 // send();
-// stop();
-websocketClient();
+// Stop();
 
 // sendMessage();
 
