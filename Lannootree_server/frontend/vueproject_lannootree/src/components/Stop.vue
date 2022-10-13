fa1742efcf2ae69e62a47a238e688a8710bd5d86
Jay D'Hulster <jaydhulster@gmail.com>
2022-10-13 15:02:54 +0200

    stop update

--- Lannootree_server/frontend/vueproject_lannootree/src/components/Stop.vue
+++ Lannootree_server/frontend/vueproject_lannootree/src/components/Stop.vue
@@ -1,7 +1,19 @@
 <script>
-  import { Stop } from '@/assets/api-connection.js';
+import { Stop } from '@/assets/api-connection.js';
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+    stop() {
+      console.log("stop")
+      // <Stop />
+    }
+  }
+}
 </script>
-    
+
 <template>
-  <button @click="Stop" >Stop</button>
+	<button @click="stop">Stop</button>
 </template>
