e19375a2de78fec26e719cbc9b5ecd7e76c330f8
Jay D'Hulster <jaydhulster@gmail.com>
2022-10-13 15:19:13 +0200

    stopper

--- Lannootree_server/frontend/vueproject_lannootree/src/components/Stop.vue
+++ Lannootree_server/frontend/vueproject_lannootree/src/components/Stop.vue
@@ -1,6 +1,17 @@
 <script>
 import { Stop } from '@/assets/api-connection.js';
-
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+    stop() {
+      // console.log("stop")
+      // <Stop/>
+    }
+  }
+}
 </script>
 
 <template>
