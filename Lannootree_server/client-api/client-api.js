73a66642fa34c343aef210baceced8d2ec3c5dbe
JensVA <jens.vanhove99@gmail.com>
2022-10-13 10:54:54 +0200

    inserted controller functions

--- Lannootree_server/client-api/client-api.js
+++ Lannootree_server/client-api/client-api.js
@@ -14,12 +14,34 @@ client.on("connect", () => {
     // ws.send("stop");
 });
 
+function pause() {
+    console.log('pause');
+    client.publish('controller/pause', JSON.stringify({"value": "pause"}));
+}
 
-// publish on server
-function onOff(onoff) {
-    console.log('onOff');
-    client.publish('controller/stop', onoff);
-    console.log('peoe');
+function togglepause() {
+    console.log('togglepause');
+    client.publish('controller/pause', JSON.stringify({"value": "togglepause"}));
+}
+
+function start() {
+    console.log('start');
+    client.publish('controller/pause', JSON.stringify({"value": "start"}));
+}
+
+function stop() {
+    console.log('stop');
+    client.publish('controller/stop', JSON.stringify({"value": "stop"}));
+}
+
+function setcolor(red, green, blue) {
+    console.log(`setcolor: (${red},${green},${blue})`);
+    client.publish('controller/setcolor', JSON.stringify({"red": red, "green": green, "blue": blue}));
+}
+
+function effect(effect_id) {
+    console.log(`effect: ${effect_id}`);
+    client.publish('controller/effect', JSON.stringify({"effect_id": effect_id}));
 }
 
 // client.subscribe('controller/stop', function (err) {
@@ -40,7 +62,7 @@ websocket.on('connection', ws => {
     ws.on("message", data => {
         console.log('Reicieved: ' . data.Tostring());
         if(data == "stop") {
-            onOff();
+            stop();
         }
     });
 
