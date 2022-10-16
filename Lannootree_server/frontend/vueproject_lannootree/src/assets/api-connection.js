3f2771c7ae5991056c96994b98556e0d09471d88
madness007 <arno.schoutteten@gmail.com>
2022-10-16 11:52:10 +0000

    added .env to repo

--- Lannootree_server/frontend/vueproject_lannootree/src/assets/api-connection.js
+++ Lannootree_server/frontend/vueproject_lannootree/src/assets/api-connection.js
@@ -1,5 +1,4 @@
-// const ws = new WebSocket(import.meta.env.VITE_FRONTEND_WEBSOCKET);
-const ws = new WebSocket('wss://lannootree.devbitapp.be/wss');
+const ws = new WebSocket(import.meta.env.VITE_FRONTEND_WEBSOCKET);
 
 var websocketactive = false; 
 export default function websocketClient() {
