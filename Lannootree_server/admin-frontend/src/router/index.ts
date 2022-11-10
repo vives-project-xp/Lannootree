26ab13c7e5094ae4e19aaac69819078b7772d694
Joey <joey@de-smet.vlaanderen>
2022-11-10 10:13:54 +0100

    Upgraded to use vuetify3 release

--- Lannootree_server/admin-frontend/src/router/index.ts
+++ Lannootree_server/admin-frontend/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import ConfigViewVue from '@/views/ConfigView.vue'
+import ConfigViewVue from '@/views/configView/ConfigView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
