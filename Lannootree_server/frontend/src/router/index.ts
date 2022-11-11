0e76511a2c5e8449c6f56829802d07fc78e4ae4e
Jay D'Hulster <jaydhulster@gmail.com>
2022-11-11 17:17:19 +0100

    upload buitton

--- Lannootree_server/frontend/src/router/index.ts
+++ Lannootree_server/frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ConfigViewVue from '@/views/configView/ConfigView.vue'
 import ControlViewVue from '@/views/frontendView/ControlView.vue'
+import UploadViewVue from '@/views/frontendView/UploadView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/controll",
       name: "controll",
       component: ControlViewVue
+    },
+    {
+      path: "/upload",
+      name: "upload",
+      component: UploadViewVue
     }
   ]
 })
