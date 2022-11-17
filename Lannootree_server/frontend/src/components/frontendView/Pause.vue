16d3c80a391dad11bd2b5e85fd1ba7593167656f
EmielCoucke <emiel.coucke@me.com>
2022-11-17 14:12:11 +0100

    fixed pause btn frontend

--- Lannootree_server/frontend/src/components/frontendView/Pause.vue
+++ Lannootree_server/frontend/src/components/frontendView/Pause.vue
@@ -3,6 +3,7 @@
   import { useClientAPIStore } from '@/stores/client.connection'
   
   const clientStore = useClientAPIStore();
+  // const current = ref()
 
 </script>
 
@@ -10,6 +11,7 @@
   <v-btn
     :icon="clientStore.status_json.pause ? 'mdi-pause' : 'mdi-play'"
     :color="clientStore.status_json.pause ? 'red' : 'green'"
-    @click="clientStore.status_json.pause = !clientStore.status_json.pause"
+    @click="clientStore.status_json.pause ? clientStore.Pause(false) : clientStore.Pause(true)"    
   />
 </template>
+
