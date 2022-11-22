009d6d11bdfa30c5d6bee0902f1641add7dbb003
madness007 <arno.schoutteten@gmail.com>
2022-11-22 18:37:29 +0000

    githook can do pull

---
+++ Lannootree_server/githook/scripts/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /Lannootree
+git pull
+# cd /Lannootree/Lannootree_server
+# echo docker-compose up -d --build frontend admin-api client-api controller effect-generator uploader-api
+# echo "Webhook received for branch '$1'"
+exit 0
