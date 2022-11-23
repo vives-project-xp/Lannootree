4400bbeb721836c92b0c78c8dc3fd8c847f893c1
madness007 <71696919+madness007@users.noreply.github.com>
2022-11-23 19:25:17 +0100

    mqtt certs in root certs folder (#23)

---
+++ certs/generate_certs.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo apt install openssl -y
+
+# Generate CA 
+openssl genrsa -out ./ca/ca.key 2048
+openssl req -new -x509 -days 3650 -key ./ca/ca.key -out ./ca/ca.crt
+
+# Generate server certificate and sign it using newly created CA
+openssl genrsa -out ./server/server.key 2048
+openssl req -new -out ./server/server.csr -key ./server/server.key
+openssl x509 -req -in ./server/server.csr -CA ./ca/ca.crt -CAkey ./ca/ca.key -CAcreateserial -out ./server/server.crt -days 3650
+
+# Generate client certificate but don't sign. because clients 
+openssl genrsa -out ./server/server.key 2048
+openssl req -new -out ./server/server.csr -key ./server/server.key
