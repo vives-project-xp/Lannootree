e1004ee9f91643a97d5d92a60ca9214d64fa6ad0
madness__007 <arno.schoutteten@gmail.com>
2022-11-23 20:15:25 +0100

    generate certs minor change

--- certs/generate_certs.sh
+++ certs/generate_certs.sh
@@ -12,5 +12,5 @@ openssl req -new -out ./server/server.csr -key ./server/server.key
 openssl x509 -req -in ./server/server.csr -CA ./ca/ca.crt -CAkey ./ca/ca.key -CAcreateserial -out ./server/server.crt -days 3650
 
 # Generate client certificate but don't sign. because clients 
-openssl genrsa -out ./server/server.key 2048
-openssl req -new -out ./server/server.csr -key ./server/server.key
+openssl genrsa -out ./client/client.key 2048
+openssl req -new -out ./client/client.csr -key ./client/client.key
