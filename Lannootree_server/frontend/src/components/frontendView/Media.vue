fc96b5685a4d07a43c92ce3360edace3dab2a9ec
Jay D'Hulster <jaydhulster@gmail.com>
2022-12-01 11:00:40 +0100

    media view start

---
+++ Lannootree_server/frontend/src/components/frontendView/Media.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import { useClientAPIStore } from '@/stores/client.connection';
+  import {ref} from 'vue';
+  
+  const clientStore = useClientAPIStore();
+  const current = ref('')
+</script>
+    
+<template>
+    <v-row
+      class="d-flex align-center justify-center"
+    >
+      <v-chip
+        v-for="label in clientStore.status_json.media"
+        :key="`chip_${label.name}`"
+        class="ma-3"
+      >
+        {{ label.name }}
+      </v-chip>
+    </v-row>
+</template>
