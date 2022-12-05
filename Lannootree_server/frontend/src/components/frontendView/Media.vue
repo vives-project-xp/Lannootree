a89711e3097c9c8da8bd618ae00313f720c280cb
Jay D'Hulster <jaydhulster@gmail.com>
2022-12-05 11:50:15 +0100

    temporary fix to button color updating

--- Lannootree_server/frontend/src/components/frontendView/Media.vue
+++ Lannootree_server/frontend/src/components/frontendView/Media.vue
@@ -8,6 +8,7 @@
   function sendId(idToSend: Number) {
   // `event` implicitly has `any` type
   clientStore.setMedia(idToSend)
+  clientStore.setMedia(idToSend)
   console.log(clientStore.status_json.active.media_id)
   }
 </script>
@@ -20,8 +21,8 @@
         v-for="label in clientStore.status_json.media"
         :key="`chip_${label.name}`"
         class="ma-3"
-        @click="sendId(label.id)"
         :color="clientStore.status_json.active.media_id == label.id ? '#00bd7e' : 'secondary'"
+        @click="sendId(label.id)"
       >
         {{ label.name }}
       </v-btn>
