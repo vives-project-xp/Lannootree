614b61efc0b13c7ed397473cf1816b8f8660f536
madness007 <arno.schoutteten@gmail.com>
2022-12-08 16:30:44 +0100

    hotfix storage was broken

--- Lannootree_server/storage/src/storage.js
+++ Lannootree_server/storage/src/storage.js
@@ -3,9 +3,8 @@ import mqtt from "mqtt";
 import * as fs from 'fs';
 import * as sqlite3 from 'sqlite3';
 const db = new sqlite3.default.Database('./db/storage.sqlite');
-dotenv.config({ path: '../.env' })
 
-import Player from "./player.js";
+dotenv.config({ path: '../.env' })
 
 // MQTT ______________________________________________________________________________________
 
@@ -21,69 +20,57 @@ var options={
   ca:caFile,
   cert: clientcrt,
   key: clientkey,
-  will: {
-    topic: "status/storage",
-    payload: "Offline",
-    retain: true
-  }
+    will: {
+        topic: "status/storage",
+        payload: "Offline",
+        retain: true
+    }
 };
 const client = mqtt.connect(options);
 
-var player = null;
 client.on('connect', function () {
-  logging("[INFO] mqtt connected")
-  client.publish('status/storage', 'Online', {retain: true});
-  client.subscribe("storage/in");
-  player = new Player(client);
+    logging("[INFO] mqtt connected")
+    client.publish('status/storage', 'Online', {retain: true});
+    client.subscribe("storage/in");
 });
 
 function logging(message, msgdebug = false){
-  if (!msgdebug) {
-    console.log(message);
-    client.publish('logs/storage', message);
-  }
-  else if(msgdebug && debug) {
-    console.log(message);
-  }
+    if (!msgdebug) {
+        console.log(message);
+        client.publish('logs/storage', message);
+    }
+    else if(msgdebug && debug) {
+        console.log(message);
+    }
 }
 
 client.on('message', function (topic, message) {
-  let data = message;
-  try {
-    data = JSON.parse(message.toString());
-  } catch (error) {
-    data = message;
-  }
-  switch (topic) {
-    case "storage/in":
-      switch(data.command) {
-        case "send_media": send_media(); break;
-        case "play": play_stream(); break;
-        case "stop": stop_stream(); break;
-      }
-  }
+    let data = message;
+    try {
+      data = JSON.parse(message.toString());
+    } catch (error) {
+      data = message;
+    }
+    switch (topic) {
+      case "storage/in":
+        switch(data.command) {
+          case "send_media": send_media(); break;
+        }
+    }
 });
 
 function send_media() {
-  let new_media = [];
-  db.serialize(() => {
-    db.all(`SELECT * FROM media`, (err, result) => {
-      if (err) {
-        console.error(err.message);
-      }
-      console.log(result)
-      for (let index = 0; index < result.length; index++) {
-        new_media.push({id: result[index].id, name: result[index].name, category: result[index].category, description: result[index].description});
-      }
-      client.publish('controller/in', JSON.stringify({"command": "media", "media": new_media}));
+    let new_media = [];
+    db.serialize(() => {
+        db.all(`SELECT * FROM media`, (err, result) => {
+          if (err) {
+            console.error(err.message);
+          }
+          console.log(result)
+          for (let index = 0; index < result.length; index++) {
+            new_media.push({id: result[index].id, name: result[index].name, category: result[index].category, description: result[index].description});
+          }
+          client.publish('controller/in', JSON.stringify({"command": "media", "media": new_media}));
+        });
     });
-  });
-}
-
-function play_stream() {
-  if(player != null) player.play("aap.gif.json");
-}
-
-function stop_stream() {
-  if(player != null) player.stop();
 }
