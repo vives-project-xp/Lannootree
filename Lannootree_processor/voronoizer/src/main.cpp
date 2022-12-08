df5995a8d3d19e60287208608b32f1db35f863c7
Joey <joey@de-smet.vlaanderen>
2022-12-08 09:05:04 +0100

    Added json-redis-formatter

--- Lannootree_processor/voronoizer/src/main.cpp
+++ Lannootree_processor/voronoizer/src/main.cpp
@@ -1,11 +1,14 @@
 #include <voroniozer.hpp>
 #include <voronoizer-aruments.hpp>
 
+#include <json-redis-formatter.hpp>
+
 int main(int argc, char* argv[]) {
   Processing::VoronoizerArguments args(argc, argv);
   
   std::shared_ptr<Processing::Formatter> formatter;
-  formatter = std::make_shared<Processing::JSONFileFormatter>("./saves/", true);
+  // formatter = std::make_shared<Processing::JSONFileFormatter>("./saves/", true);
+  formatter = std::make_shared<Processing::JsonRedisFormatter>("redis://localhost:6379");
 
   Processing::Voronoizer voroizer(args.get_frame_provider(), formatter, "./config.json");
   voroizer.start(args.get_thread_count());
