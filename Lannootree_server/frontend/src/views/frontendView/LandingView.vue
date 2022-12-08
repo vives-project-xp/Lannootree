6d70dd08dd2fa7c73321191b1ea707f551989fae
EmielCoucke <emiel.coucke@me.com>
2022-12-08 10:13:51 +0100

    infopage lannootree

--- Lannootree_server/frontend/src/views/frontendView/LandingView.vue
+++ Lannootree_server/frontend/src/views/frontendView/LandingView.vue
@@ -28,38 +28,46 @@
 
       
       <div>
-        <h2 class="d-flex justify-center align-center">
+        <h1 class="text-center mt-16">
+          Informatie
+        </h1>
+
+        <h2 class="text-center mt-4">
           Madia pagina
         </h2>
-        <p class="d-flex justify-center align-center">
+        <p class="text-center">
           In de media pagina kan je verschillende gifs sturen naar de Lannnootree.
         </p>
 
-        <h2 class="d-flex justify-center align-center">
+        <h2 class="text-center mt-4">
           Upload
         </h2>
-        <p class="d-flex justify-center align-center">
+        <p class="text-center">
           Op deze pagina kan je gifs en fotos gaan importeren, deze gaan dan afspelen op de Lannootree.
         </p>
 
-        <h2 class="d-flex justify-center align-center">
+        <h2 class="text-center mt-4">
           ConfigPanel
         </h2>
-        <p class="d-flex justify-center align-center">
+        <p class="text-center">
           Hier kan je gaan aanduiden hoeveel panelen er verbonden zijn en welke paneel met welk paneel verbonden is.
         </p>
 
-        <h2 class="d-flex justify-center align-center">
+        <h2 class="text-center mt-4">
           LogPanel
         </h2>
-        <p class="d-flex justify-center align-center">
+        <p class="text-center">
           Met dit logpaneel kan je zien welke containers wel of niet online zijn. Er worden naar deze pagina berichten gestuurd met info over de containers.
         </p>
       </div>
-          
 
-     
-      
+      <h1 class="text-center mt-4">
+        Blogpost
+      </h1>
+
+      <div class="text-center">
+        <img src="../../assets/frontendView/Blogpost.png" alt="blogpost"/>
+      </div>
   </div>
 </template>
 
