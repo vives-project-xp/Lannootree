469635672d2ab20528fd29099ab2e26c16bc13f7
Joey <joey@de-smet.vlaanderen>
2022-12-14 09:51:07 +0100

    Used correct rendering & hopefully the corre gpio pins

--- Lannootree_firmware/led-driver/ProjectFiles/src/led_driver_thread.cpp
+++ Lannootree_firmware/led-driver/ProjectFiles/src/led_driver_thread.cpp
@@ -26,29 +26,48 @@ namespace Lannootree {
   }
 
   void LedDriverThread::loop(void) {
-    int ret;
+    int ret = 0;
+
+    std::string channel_a = "CA";
+    std::string channel_b = "CB";
 
     while (_running) {
+      // Render channel A Than Channel B
+      auto controller_a = _controllers->at(0);
 
-      for (auto& [channel, led_buffer] : *_channel_mem) {
-        ws2811_t* controller = _controllers->at(channel.substr(0, 1) == "CA" ? 0 : 1);
-        
-        bool success = led_buffer->mem_read(controller->channel[channel.substr(1, 2) == "0" ? 0 : 1].leds);
+      _channel_mem->at("CA0")->mem_read(controller_a->channel[0].leds);
+      _channel_mem->at("CA1")->mem_read(controller_a->channel[1].leds);
 
-        if (!success) break;
-        
-        // Because ws2811 driver is writen in C this mess exists
-        ret = ws2811_render(controller, [](void* arg, void* channel) {
-          
-          auto _this = (LedDriverThread*) arg;
-          _this->_channel_mem->at(*(std::string*) channel)->swap();
+      ret = ws2811_render(controller_a, [](void* arg, void* channel) {
 
-        }, this, (void*) &channel);
+        auto _this = (LedDriverThread*) arg;
+        _this->_channel_mem->at(*(std::string*) channel + "0")->swap();
+        _this->_channel_mem->at(*(std::string*) channel + "1")->swap();
 
-        if (ret != WS2811_SUCCESS) {
-          std::string error = ws2811_get_return_t_str((ws2811_return_t)ret);
-          error_log("Failed to render " << error);
-        }
+      }, this, &channel_a);
+
+      if (ret != WS2811_SUCCESS) {
+        std::string error = ws2811_get_return_t_str((ws2811_return_t)ret);
+        error_log("Failed to render " << error);
+      }
+
+
+      auto controller_b = _controllers->at(1);
+
+      _channel_mem->at("CB0")->mem_read(controller_b->channel[0].leds);
+      _channel_mem->at("CB1")->mem_read(controller_b->channel[1].leds);
+
+      ret = ws2811_render(controller_a, [](void* arg, void* channel) {
+
+        auto _this = (LedDriverThread*) arg;
+        _this->_channel_mem->at(*(std::string*) channel + "0")->swap();
+        _this->_channel_mem->at(*(std::string*) channel + "1")->swap();
+
+      }, this, &channel_b);
+
+      if (ret != WS2811_SUCCESS) {
+        std::string error = ws2811_get_return_t_str((ws2811_return_t)ret);
+        error_log("Failed to render " << error);
       }
     }
   }
