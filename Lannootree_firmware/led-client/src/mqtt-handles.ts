da89066809637122a814b737b421dc4be5b05cd3
Joey De Smet <joey@de-smet.vlaanderen>
2022-12-21 10:36:27 +0100

    local files

--- Lannootree_firmware/led-client/src/mqtt-handles.ts
+++ Lannootree_firmware/led-client/src/mqtt-handles.ts
@@ -7,27 +7,20 @@ import { gifPlayer } from './temporary/gif-player.js'
 const config = JSON.parse(fs.readFileSync('./config.json').toString());
 
 export function pause_leds() {
-  // gifPlayer.pause();
   streamPlayer.pause();
 }
 
 export function play_leds() {
   streamPlayer.play();
-  // gifPlayer.play();
 }
 
 export function stop_leds() {
   streamPlayer.stop();
-  // gifPlayer.stop();
-}
-
-export function play_gif(data: any) {
-  // gifPlayer.set_gif(data.gif_number);
 }
 
 export function set_color(data: any) {
-  gifPlayer.stop();
   streamPlayer.stop();
+
 
   let cString = new Array<number>(config.channels.CA0.ledCount * 3);
   
