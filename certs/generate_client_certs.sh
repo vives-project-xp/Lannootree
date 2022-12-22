d9bfa5dffab235a6ea2f96d5366160faf3552d78
Arno <arno.schoutteten@gmail.com>
2022-12-22 08:32:09 +0000

    cert generation ca fixed

--- certs/generate_client_certs.sh
+++ certs/generate_client_certs.sh
@@ -13,9 +13,12 @@ echo '\n\n\n\nEnter CN (common name) only lowercase and no spaces. This is the n
 read CN
 
 echo 'generating client key and csr in clinet folder...'
-# Generate client certificate but don't sign. because clients 
+# Generate client certificate but don't sign. because clients
 openssl genrsa -out ./client/client.key 2048
 openssl req -new -out ./client/${CN}.csr -key ./client/client.key -subj "/CN=${CN}/O=Elektronica-ICT Vives/C=BE"
 
 echo "done! \ngive ${CN}.csr to the server admin and ask for a signed crt"
 echo "then rename the  ${CN}.crt to client.crt and place it in the client folder"
+
+echo "\n\n This is the CSR"
+cat ./client/${CN}.csr
--- certs/sign_new_clients.sh
+++ certs/sign_new_clients.sh
@@ -4,6 +4,8 @@ for file in $(find signed_clients -name "*.csr")
 do
   filename=$(basename "$file" ".csr")
   openssl x509 -req -in $file -CA ./ca/ca.crt -CAkey ./ca/ca.key -CAcreateserial -out ./signed_clients/$filename.crt -days 3650
+  echo "Certificate for $filename:"
+  cat ./signed_clients/$filename.crt
   mv "$file" "signed_clients"
 done
 
