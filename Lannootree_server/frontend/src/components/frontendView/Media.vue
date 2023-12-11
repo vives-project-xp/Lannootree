8dc8bbef795e7342b6413b01b8692c7c445b0459
jayDhulster <jaydhulster@gmail.com>
2023-12-11 08:40:11 +0000

    Upload notifications on media and upload page

--- Lannootree_server/frontend/src/components/frontendView/Media.vue
+++ Lannootree_server/frontend/src/components/frontendView/Media.vue
@@ -1,9 +1,47 @@
 <script setup lang="ts">
   import { useClientAPIStore } from '@/stores/client.connection';
-  import {ref} from 'vue';
+  import { ref, computed } from 'vue';
+  import { useToast } from "vue-toastification";
+  import { useToastStore } from '@/stores/ToastStore';
   
   const clientStore = useClientAPIStore();
+  const toastStore = useToastStore();
+  const toast = useToast();
   const current = ref('')
+  
+
+  const calculateProgress = () => {
+    const frame = clientStore.render_status_json.frame || 0;
+    const totalFrames = clientStore.render_status_json.totalFrames || 1; // Prevent division by zero
+    const progress = (frame / totalFrames) * 100;
+    if (frame == 1) {
+      // when more than 1 frame has been done, reset shownstate so that notifications for following uploads can be shown.
+      toastStore.setSuccessToastShown(false);        
+    }
+    if (frame == 1 && !toastStore.getUploadToastShown()) {
+      toastStore.setUploadToastShown(true);
+      toast.info("File uploaded, rendering!", {
+        timeout: 5000
+      });
+    }
+
+    if (progress >= 100 && !toastStore.getSuccessToastShown()) {
+      toast.success("Congratulations! Your file has been successfully uploaded to the server.");
+      toastStore.setSuccessToastShown(true);
+      toastStore.setUploadToastShown(false);
+    }
+    return progress;
+  };
+
+  const renderStatus = computed(() => {
+    const frame = clientStore.render_status_json.frame;
+    const totalFrames = clientStore.render_status_json.totalFrames;
+    if (totalFrames === 0) {
+      return "";
+    }
+    return `${frame}/${totalFrames}`;
+  });
+  
   
   function sendId(idToSend: Number) {
   // `event` implicitly has `any` type
@@ -27,4 +65,6 @@
         {{ label.name }}
       </v-btn>
     </v-row>
+    <!-- This hidden element below enables notifications -->
+    <p class="d-none">{{ calculateProgress() }}</p>
 </template>
